5331a3755c9cddbc1317a1972d3edd667b6305f7
ertuSa <19247355+ertuSa@users.noreply.github.com>
2018-06-09 20:43:23 +0200

    Added comments.

--- AccelerationSensor/main.c
+++ AccelerationSensor/main.c
@@ -6,7 +6,6 @@
  */ 
 
 #define F_CPU				16000000UL
-#define BAUD				9600
 #define MYUBRR				0x67 /* decimal 103 = baud rate 9600 */
 
 #define ACCEL_TRESHOLD		31
@@ -15,10 +14,8 @@
 #include <util/delay.h>
 #include "function.h"
 
-int8_t temp;
-uint8_t state;
-int8_t readBuffer;
-uint8_t writeBuffer;
+int8_t temp, readBuffer;
+uint8_t state, writeBuffer;
 accels_ts accelsTemp;
 
 accels_ts accels = {0, 0, 0};
@@ -27,34 +24,40 @@ int main(void)
 {
 	state = ACCEL_INIT_STATE;
 	
-	/* configuration of uC, I/O pins */
-	DDRB |= _BV(5);
-	//DDRC |= _BV(PC4) | _BV(PC5);
-	PRR &= ~( _BV(PRTWI) | _BV(PRUSART0) );
-	
-	/* Initialization of USART communication */
-	USART_Init(MYUBRR);
-	
     while (1) 
     {
 		
 		switch(state)
 		{
-		case ACCEL_INIT_STATE:
-			I2C_Init();
-			state = ACCEL_CONFIG_STATE;
-			_delay_ms(100);
-			for (int x = 0; x < 4; x++)
-			{
-				_delay_ms(250);
-				PORTB ^= _BV(5);
-			}
 			
+		/* State that initiate I/O and communication ports of uC */
+		case ACCEL_INIT_STATE:
+			/* configuration of uC, I/O pins */
+			DDRB |= _BV(5);
+			//DDRC |= _BV(PC4) | _BV(PC5);
+			PRR &= ~( _BV(PRTWI) | _BV(PRUSART0) );
+			/* Initialization of USART communication */
+			USART_Init(MYUBRR);
+			/* Initialization of I2C communication */
+			I2C_Init();
+			
+			/* Changing the state of state machine */
+			state = ACCEL_CONFIG_STATE;
+			
+			//TODO: This part of code need to be deleted before release
+			/* For debugging only ***********/ 
+			_delay_ms(100);					//
+			for (int x = 0; x < 4; x++)		//
+			{								//
+				_delay_ms(250);				//
+				PORTB ^= _BV(5);			//
+			}								//
+			/********************************/
 		break;
 		
-		/* Configuration of slave device */	
+		/* State that configure slave device */	
 		case ACCEL_CONFIG_STATE:					
-			I2C_SendStartAndSelect(LIS3DH_W);
+			I2C_SendStartAndSelect(LIS3DH_W);	//
 			I2C_SendByte(CTRL_REG1 | ADR_INC_MASK);
 			I2C_SendByte(0x5F);		// CTRL_REG1 = 0x5F
 			I2C_SendByte(0x00);		// CTRL_REG2 = 0x00
@@ -73,37 +76,45 @@ int main(void)
 			I2C_Stop();
 			if(0x33 == readBuffer)
 			{
-				for(uint8_t x = 0; x < 20; x++)
+				//TODO: This part of code need to be deleted before release
+				/* For debugging only ***********/
+				for(uint8_t x = 0; x < 20; x++)	//
+				{								//
+					PORTB ^= _BV(PB5);			//
+					_delay_ms(100);				//
+				}								//
+				/********************************/
+				
+				/* Get acceleration values from each axis */
+				I2C_SendStartAndSelect(LIS3DH_W);
+				I2C_SendByte(STATUS_REG_AUX);
+				I2C_SendStartAndSelect(LIS3DH_R);
+				readBuffer = I2C_ReceiveDataByte_NACK();
+				if(_BV(DA321) & readBuffer)
 				{
-					PORTB ^= _BV(PB5);
-					_delay_ms(100);
+					I2C_SendStartAndSelect(LIS3DH_W);
+					I2C_SendByte(OUT_X_L | 0x80);
+					I2C_SendStartAndSelect(LIS3DH_R);
+					accels.x = I2C_ReceiveDataBytes_ACK();
+					accels.y = I2C_ReceiveDataBytes_ACK();
+					accels.z = I2C_ReceiveDataByte_NACK();
+					I2C_Stop();
 				}
+				state = ACCEL_RUN_STATE;
 			}
 			else
 			{
-				PORTB |= _BV(PB5);
-				_delay_ms(2000);
+				//TODO: This part of code need to be deleted before release
+				/* For debugging only ***********/
+				PORTB |= _BV(PB5);				//
+				_delay_ms(2000);				//
+				/********************************/
 			}
 			
-			/* Get acceleration values from each axis */
-			I2C_SendStartAndSelect(LIS3DH_W);
-			I2C_SendByte(STATUS_REG_AUX);
-			I2C_SendStartAndSelect(LIS3DH_R);
-			readBuffer = I2C_ReceiveDataByte_NACK();
-			if(_BV(DA321) & readBuffer)
-			{
-				I2C_SendStartAndSelect(LIS3DH_W);
-				I2C_SendByte(OUT_X_L | 0x80);
-				I2C_SendStartAndSelect(LIS3DH_R);
-				accels.x = I2C_ReceiveDataBytes_ACK();
-				accels.y = I2C_ReceiveDataBytes_ACK();
-				accels.z = I2C_ReceiveDataByte_NACK();
-				I2C_Stop();
-			}
 			
-			state = ACCEL_RUN_STATE;
 		break;
 			
+		/* State that control get acceleration values from slave device */
 		case ACCEL_RUN_STATE:
 			I2C_SendStartAndSelect(LIS3DH_W);
 			I2C_SendByte(STATUS_REG_AUX);
@@ -124,6 +135,7 @@ int main(void)
 			}	
 		break;
 			
+		/* State that compare accel values from memory and values get from acceleration sensor */	
 		case ACCEL_WORKING_STATE:
 			USART_Transmit(accelsTemp.x);
 			USART_Transmit(accelsTemp.y);
